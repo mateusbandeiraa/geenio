e59febb0d260ec68f9fb4ae59ac099083c5c7199
mateusbandeiraa <mateusbandeiraa@gmail.com>
2022-01-20 18:30:36 -0300

    Remove unused score store

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import { createApp } from "vue";
 import { createStore } from "vuex";
-import { score } from "./store/score";
 import { questions } from "./store/questions";
 import App from "./App.vue";
 
 const store = createStore({
   modules: {
-    score,
     questions
   },
 });
