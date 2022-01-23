39e4b2967ac4884abcc04ea0237829989e39504a
Mateus Bandeira <mateusbandeiraa@gmail.com>
2022-01-23 18:20:50 -0300

    Change question shuffling algorithm (#8)
    The new algorithm avoids repeating questions from recent games.

--- src/store/questions.js
+++ src/store/questions.js
@@ -1,13 +1,12 @@
 import Question from "../model/Question";
 import questionsCSV from "!raw-loader!../assets/questions.csv";
 import { shuffle } from "shuffle-seed";
-import dayjs from 'dayjs' 
+import dayjs from "dayjs";
 import Papa from "papaparse";
 
-const tomorrow = dayjs().add(1, 'day').startOf('day');
+const tomorrow = dayjs().add(1, "day").startOf("day");
 // First game went live on jan 20th 2022.
-const gameNumber = dayjs().diff(dayjs('2022-01-20'), 'days') + 1;
-
+const gameNumber = dayjs().diff(dayjs("2022-01-20"), "days") + 1;
 export const questions = {
   state: () => ({
     nextGameDate: tomorrow,
@@ -23,13 +22,25 @@ export const questions = {
       const currentQuestion = context.getters.getCurrentQuestion;
       currentQuestion.selectAlternative = alternative;
     },
-    loadQuestions({commit, state}) {
+    loadQuestions({ commit, state }) {
       const allQuestions = Papa.parse(questionsCSV, { header: true }).data;
-      const todaysQuestions = shuffle(
-        allQuestions,
-        state.gameNumber.toString(2)
-      )
-        .slice(0, 5)
+
+      const totalQuestions = allQuestions.length;
+      const maxDays = Math.floor(totalQuestions / 5); // 5 questions per day
+      const gameNumber = state.gameNumber - 1; // Games are 1-indexed but maths here are easier with zero-indexing
+
+      const cycleNumber = Math.floor(gameNumber / maxDays); // How many times we cycled through all questons
+
+      const gameNumberInCycle = gameNumber - (cycleNumber * maxDays);
+      const firstQuestionIndex = (gameNumberInCycle) * 5;
+
+      /* If I want to force the change of the order of the questions,
+       * I can create a new deploy with a different salt. */
+      const shuffleSalt = 'NaCl';
+      const hash = cycleNumber.toString(2) + shuffleSalt;
+
+      const todaysQuestions = shuffle(allQuestions, hash)
+        .slice(firstQuestionIndex, firstQuestionIndex + 5)
         .map((questionFromCSV) => {
           return new Question(questionFromCSV);
         });
