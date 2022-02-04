e073b37707d78fa3a19341cf4d98e9f6ec983aa3
mateusbandeiraa <mateusbandeiraa@gmail.com>
2022-02-04 19:25:15 -0300

    Add and configure Spring Security

---
+++ backend/src/main/java/dev/bandeira/geenio/cortex/config/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package dev.bandeira.geenio.cortex.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// No session will be created or used by spring security
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //
+				.and().httpBasic() //
+				.and() //
+				.authorizeRequests().antMatchers("/api/public/**").permitAll() //
+				.anyRequest().authenticated() // protect all other requests
+				.and().csrf().disable(); // disable cross site request forgery, as we don't use
+											// cookies - otherwise ALL PUT, POST, DELETE will get
+											// HTTP 403!
+	}
+}
